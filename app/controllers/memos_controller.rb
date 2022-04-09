f02280984e9a3fc57d74533693ed838ac96aa60b
rabi1924 <rabi2024@gmail.com>
2022-04-09 22:04:41 +0900

    一覧表示の順序変更

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -3,7 +3,7 @@ class MemosController < ApplicationController
   before_action :set_memo, only: [:show, :edit, :update, :destroy]
 
   def index
-    @memos = Memo.all
+    @memos = Memo.all.order('created_at DESC')
   end
 
   def new
