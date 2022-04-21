142370e2ab94788664eb21e1ce6b379f3be8c3c0
rabi1924 <rabi2024@gmail.com>
2022-04-22 01:04:08 +0900

    Rubocopモレ②

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -10,7 +10,7 @@ class Memo < ApplicationRecord
 
   def self.search(search)
     if search != ''
-      Memo.includes(:user).where(['title LIKE(?) OR nickname LIKE(?)', "%#{search}%","%#{search}%"]).references(:user)
+      Memo.includes(:user).where(['title LIKE(?) OR nickname LIKE(?)', "%#{search}%", "%#{search}%"]).references(:user)
     else
       Memo.all
     end
